b9f27f46680932bce8a5ed77bdbd5e690320dd6a
Celia <celia.letellier@gmail.com>
2023-12-05 17:04:55 +0100

    5/12/23 crea repo

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+/* let arrowLeft = document.getElementById('arrow_left');
+let arrowRight = document.getElementById('arrow_right');
+
+arrowLeft.addEventListener('click', () => {
+    console.log('Flèche gauche cliquée');
+});
+arrowRight.addEventListener('click', () => {
+    console.log('Flèche droite cliquée');
+}); */
