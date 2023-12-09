7d359ef38f0f1253f9a685b0870d5a696f48dfab
Celia <celia.letellier@gmail.com>
2023-12-09 13:09:22 +0100

    9/12/23 1er essai projet 8

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,48 @@ const slides = [
 	}
 ]
 
-/* let arrowLeft = document.getElementById('arrow_left');
-let arrowRight = document.getElementById('arrow_right');
+// Liste des variables nécessaires au carrousel
+const arrowRight = document.getElementById('next');
+const arrowLeft = document.getElementById('prev');
+const bannerImg = document.querySelector('.banner-img');
+const tagLine = document.querySelector('#banner p');
+const dots = document.querySelectorAll('.dots .dot');
 
-arrowLeft.addEventListener('click', () => {
-    console.log('Flèche gauche cliquée');
-});
+let currentSlide = 0; // Index du slide actuel
+
+function updateSlide(index) {
+    currentSlide = index;
+
+    // // Mettre à jour le bullet, l'image et le texte correspondant à la slide actuelle
+    dots.forEach((dot, idx) => {
+        if (idx == currentSlide) {
+            dot.classList.add('dot_selected');
+        } else {
+            dot.classList.remove('dot_selected');
+        }
+    });
+    bannerImg.src = './assets/images/slideshow/' + slides[currentSlide].image;
+    tagLine.innerHTML = slides[currentSlide].tagLine;
+}
+
+// Changement d'affichage au clic sur la flèche droite
 arrowRight.addEventListener('click', () => {
-    console.log('Flèche droite cliquée');
-}); */
+    if (currentSlide == slides.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+    updateSlide(currentSlide);
+    console.log('Clic flèche droite');
+});
+
+// Changement d'affichage au clic sur la flèche gauche
+arrowLeft.addEventListener('click', () => {
+    if (currentSlide == 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide--;
+    }
+    updateSlide(currentSlide);
+    console.log('Clic flèche gauche');
+});
